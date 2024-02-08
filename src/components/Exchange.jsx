68dad9b66834a0abd3387f921a2a8e2f42d81f68
Ernesto Quito Gonzales <quito.gonz.ern@gmail.com>
2024-02-07 22:35:45 -0500

    Actualización de repositorio nuevos cambios

---
+++ src/components/Exchange.jsx
@@ -0,0 +1,9 @@
+export function Exchange() {
+    return (
+        <section>
+            <h1>Ingresar el tipo de cambio para hoy</h1>
+            <div></div>
+            <footer></footer>
+        </section>
+    )
+}
